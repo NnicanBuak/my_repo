1bc910a0f17b18cc892ab922768915a7a81767ea
NnicanBuak <mybailengdb@gmail.com>
2023-09-17 20:35:51 +0800

    add subtasks support & refactoring

--- 2_1/1 (09.15.23).py
+++ 2_1/1 (09.15.23).py
@@ -1,57 +1,68 @@
+from typing import Union, Callable
 import inspect
 import os
 import platform
 
-def clear_console():
-    system = platform.system()
-    if system == 'Windows':
-        os.system('cls')
-    else:
-        os.system('clear')
-
-class Task():
-    last_used_id = 1
-
-    def __init__(self, function, name, description):
-        self.id = Task.last_used_id
-        Task.last_used_id += 1
-
+class Task:
+    def __init__(self, function: Callable, id: int, name: str, description: str):
         self.function = function
+        self.id = id
         self.name = name
         self.description = description
+        self.subtasks = []
+
+class SubTask(Task):
+    def __init__(self, function: Callable, parent_id: int, id: int, name: str, description: str):
+        self.parent_id = parent_id
+        super().__init__(function, id, name, description)
 
 class TaskManager:
-    def __init__(self):
-        '''
-        Инициализация менеджера задач.
-        '''
+    last_used_id = 0
+
+    def __init__(self, ui):
         self.tasks = []
+        self.ui = ui
 
-    def add_task(self, task_function, task_name, task_description):
-        '''
-        Добавление задачи в менеджер.
+    def get_task(self, id_chain: Union[list[int], int]) -> Union[Task, None]:
+        if isinstance(id_chain, int):
+            id_chain = [id_chain]
 
-        :param task_function: Функция, выполняющая задачу.
-        :param task_name: Название задачи.
-        :param task_description: Описание задачи.
-        '''
-        task = Task(task_function, task_name, task_description)
+        current_task = None
+        for id in id_chain:
+            if current_task is None:
+                current_task = next((t for t in self.tasks if t.id == id), None)
+            else:
+                current_task = next((st for st in current_task.subtasks if st.id == id), None)
+            if current_task is None:
+                break
+        if current_task == None:
+            return
+        return current_task
+
+    def add_task(self, function: Callable, name: str, description: str):
+        self.last_used_id += 1
+        task = Task(function, self.last_used_id, name, description)
         self.tasks.append(task)
 
-    def run_task(self, task: object):
-        '''
-        Запуск задачи.
+    def add_subtask(self, function: Callable, parent_id: int, name: str, description: str):
+        parent_task = self.get_task(parent_id)
+        if parent_task is not None:
+            subtask_id = len(parent_task.subtasks) + 1
+            subtask = SubTask(function, parent_id, subtask_id, name, description)
+            parent_task.subtasks.append(subtask)
+            self.last_used_id += 1
+        else:
+            print(f"|Ошибка: Родительская задача с ID {parent_id} не найдена|")
 
-        :param task: Задача.
-        '''
-        clear_console()
+    def run_task(self, task: Task):
+        self.ui.clear_console()
         print(f"Задача: {task.name}")
         print(f"Описание: {task.description}", '\n')
 
         argspec = inspect.getfullargspec(task.function)
+        input_args = {}
         if argspec.args:
-            input_args = {}
-            print('[Введите значения для аргументов]')
+            print('|Введите значения для аргументов|')
             for arg in argspec.args:
                 arg_type = None
 
@@ -67,50 +78,131 @@ class TaskManager:
                             input_args[arg] = user_input
                         break
                     except ValueError:
-                        print(f"[Ошибка: Не удалось преобразовать введенное значение в тип {arg_type.__name__}. Попробуйте еще раз]")
+                        print(f"|Ошибка: Не удалось преобразовать введенное значение в тип {arg_type.__name__ if arg_type else 'тип не указан'}. Попробуйте еще раз|")
 
         try:
             return task.function(**input_args)
         except Exception as e:
-            print(f"[Ошибка выполнения задачи {task.name}: {e}]")
+            print(f"|Ошибка выполнения задачи {task.name}: {e}|")
             return
 
-# Создаём функции, решающие задачи
+    def display_tasks(self):
+        print('Доступные задачи:')
+        print('—————————————————')
+        for task in self.tasks:
+            print(f"{task.id}: {task.name}")
+        print('—————————————————')
+
+    def display_subtasks(self, parent_task: Task):
+        print(f"Пункты задачи {parent_task.id}:")
+        print('—————————————————')
+        for task in parent_task.subtasks:
+            print(f"{task.id}: {task.name}")
+        print('—————————————————')
+
+    def input_task(self):
+        while True:
+            try:
+                self.ui.clear_console()
+                self.ui.display_message()
+                self.display_tasks()
+                task_id = int(input('Введите номер задачи: '))
+                if task_id < 1:
+                    self.ui.set_message('Ошибка: Введён некорректный id задачи')
+                    continue
+
+                selected_task = self.get_task(task_id)
+                if selected_task:
+                    if selected_task.subtasks:
+                        while True:
+                            self.ui.clear_console()
+                            self.display_subtasks(selected_task)
+                            subtask_id = int(input('Введите номер подзадачи: '))
+                            if subtask_id < 1:
+                                self.ui.set_message('Ошибка: Введён некорректный id подзадачи')
+                                continue
+
+                            selected_subtask = self.get_task([task_id, subtask_id])
+                            if selected_subtask:
+                                return selected_subtask
+                            else:
+                                self.ui.set_message('Ошибка: Подзадача с указанным id не найдена')
+                    else:
+                        return selected_task
+                else:
+                    self.ui.set_message('Ошибка: Задача с указанным id не найдена')
+
+            except ValueError:
+                self.ui.set_message('Ошибка: Введён некорректный id задачи или подзадачи, id - целое число')
+                continue
+            except KeyboardInterrupt:
+                print('\nclosed')
+                break
+
+class ConsoleUI:
+    def __init__(self):
+        self.message = 'Ctrl+C/Del для завершения программы'
+
+    def set_message(self, message):
+        self.message = message
+
+    def display_message(self):
+        print('|' + self.message + '|' + '\n')
+
+    def clear_console(self):
+        system = platform.system()
+        if system == 'Windows':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+# Основная программа
+def main():
+    ui = ConsoleUI()
+    manager = TaskManager(ui)
+    # Добавление задач в менеджер
+    manager.add_task(task1, 'Обмен значениями переменных', 'Составьте программу обмена значениями трех переменных a, b, и c, так чтобы b получила значение c, c получила значение a, а a получила значение b.')
+    manager.add_task(task2, 'Проверка ввода двух чисел и их сумма', 'Пользователь вводит два числа. Проверьте, что введенные данные - это числа. Если нет, выведите ошибку. Если да, то выведите их сумму.')
+    manager.add_subtask(task2_1, 2, 'Проверка ввода n чисел и их сумма', 'Доработайте задачу 2.1 так, чтобы пользователь мог вводить n разных чисел, а затем выведите их сумму. Предоставьте возможность пользователю ввести значение n.')
+
+    # Основной цикл
+    while True:
+        manager.ui.clear_console()
+        task = manager.input_task()
+        result = None
+        if task:
+            result = manager.run_task(task)
+        try:
+            if result:
+                input(f"Результат: {result}\n[Enter для закрытия задачи]")
+            else:
+                input(f"[Enter для закрытия задачи]")
+        except KeyboardInterrupt:
+            print('\nclosed')
+            break
+# Функции решающие задачи
 def task1(a: int, b: int, c: int):
     a, b, c = b, c, a
     return f"a = {a}, b = {b}, c = {c}"
 
-# Создаем менеджер и добавляем задачи
-manager = TaskManager()
-manager.add_task(task1, 'Обмен значениями переменных', 'Составьте программу обмена значениями трех переменных a, b, и c, так чтобы b получила значение c, c получила значение a, а a получила значение b.')
+def task2(number1, number2):
+    while True:
+        try:
+            summ = int(number1) + int(number2)
+            print('Все введённые значения — числа')
+            return summ
+        except ValueError:
+            print('Одно или несколько введённых значений — не числа')
+            break
+
+def task2_1(numbers: list):
+    for value in numbers:
+        if not isinstance(value, (int, float)):
+            print('Одно или несколько введённых значений — не числа')
+            return
+    print('Все введённые значения — числа')
+    return sum(numbers)
 
 # Исполнение программы
 if __name__ == '__main__':
-    message = 'Ctrl+C/Del для завершения программы'
-    while True:
-        try:
-            clear_console()
-            print(f"[{message}]", '\n')
-            print('Доступные задачи:')
-            print('—————————————————')
-            for task in manager.tasks:
-                print(f"{task.id}: {task.name}")
-            print('—————————————————')
-
-            task_id = int(input('Введите номер задачи: '))
-
-            if task_id < 1:
-                message = '[Ошибка: Введён некорректный id задачи]'
-                pass
-
-            task = next((t for t in manager.tasks if t.id == task_id), None)
-            result = manager.run_task(task)
-            input(f"Результат: {result}\n[Enter для закрытия]")
-
-        except ValueError:
-            message = '[Ошибка: Введён некорректный id задачи (требуется целое число)]'
-        except KeyboardInterrupt:
-            print('\n[Программа завершена по запросу пользователя]')
-            break
-else:
-    print('[Предупреждение: Это консольное приложение, запустите основной файл Python]')
+    main()
