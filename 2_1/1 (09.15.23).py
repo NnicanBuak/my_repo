d802690c61d3549f1edaa5fb2ffea5381bfc34a6
NnicanBuak <mybailengdb@gmail.com>
2023-09-27 00:37:34 +0800

    fix

--- 2_1/1 (09.15.23).py
+++ 2_1/1 (09.15.23).py
@@ -475,7 +475,7 @@ def task_11_1(array: list[int]) -> str:
     result1, time1, memory1 = measure_memory_time(last_element_1)
     result2, time2, memory2 = measure_memory_time(last_element_2)
     result3, time3, memory3 = measure_memory_time(last_element_3)
-    return f"последний элемент: {result1}, время способа 1: {format_float(time1)}, время способа 2: {format_float(time2)}, время способа 3: {format_float(time3)}"
+    return f"последний элемент: {result1}, время способа 1: {format_float(time1) if time1 else time1}, время способа 2: {format_float(time2) if time2 else time2}, время способа 3: {format_float(time3) if time3 else time3}"
 
 def task_12_1(array: list[int]) -> list[int]:
     return array[::-1]
