74d16082d1ab47df09634a96a627ffab178d5249
NnicanBuak <mybailengdb@gmail.com>
2023-12-04 22:20:53 +0800

    update reading from file by input path

--- 2_1/TriFinder/max-min-triangles.py
+++ 2_1/TriFinder/max-min-triangles.py
@@ -3,17 +3,30 @@ from pycoordsplain.points import Point, PointsDraw
 from pycoordsplain.triangles import Triangle, TrianglesDraw, min_max_triangle
 
 
-from matplotlib.widgets import Button
+from matplotlib.widgets import Button, TextBox
 import matplotlib.pyplot as plt
 from matplotlib.tri import Triangulation
 
+
+def read_pointslist_from_file(data_path: str):
+    points.list = []
+    with open(data_path) as pointlist:
+        # "[0, 1][2, 5][3, 0]"" ...
+        splitted_pointlist: list[str] = pointlist.read().strip("[]").split("][")
+        for splitted_point in splitted_pointlist:
+            points.add_point(
+                int(splitted_point.split(",")[0]), int(splitted_point.split(",")[1])
+            )
 
 def distance_squared(point1, point2):
     return (point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2
 
 
-def on_clicked(event):
+def on_findbutton_clicked(event):
     min_max_triangle(points.list)
+
+def on_pointlistpath_submit(event):
+    read_pointslist_from_file(event.text)
 
 
 if __name__ == "__main__":
@@ -23,26 +36,17 @@ if __name__ == "__main__":
     plt.rcParams["figure.figsize"] = (8, 8)
 
     figure, axes = plt.subplots()
-    figure.suptitle("Задача: Поиск max и min треугольников между заданными точками")
+    figure.suptitle("\n\nЗадача: Поиск max и min треугольников между заданными точками")
     axes.set_title("Координатная плоскость")
     axes.set_xlabel("Ось X")
     axes.set_ylabel("Ось Y")
-    button = Button(plt.axes((0.9, 0.95, 0.1, 0.05)), "Поиск", color="g")
-    button.on_clicked(on_clicked)
-
+    find_button = Button(plt.axes((0.9, 0.95, 0.1, 0.05)), "Поиск", color="g")
+    find_button.on_clicked(on_findbutton_clicked)
+    pointslist_path = TextBox(plt.axes((0.18, 0.95, 0.3, 0.05)), "Путь к списку точек:", "./2_1/data/plist.txt")
+    pointslist_path.on_submit(on_pointlistpath_submit)
     points = PointsDraw(axes, scale=10)
 
-    data_path: str = "./2_1/data/plist.txt"
-    # data_path_input: str = input("Введите путь к файлу с координатами точек: ")
-    # if data_path_input:
-    #     data_path = data_path_input
-    with open(data_path) as pointlist:
-        # "[0, 1][2, 5][3, 0]"" ...
-        splitted_pointlist: list[str] = pointlist.read().strip("[]").split("][")
-        for splitted_point in splitted_pointlist:
-            points.add_point(
-                int(splitted_point.split(",")[0]), int(splitted_point.split(",")[1])
-            )
+    read_pointslist_from_file(pointslist_path.text)
 
     points.adjust_axis_limits()
     plt.show()
