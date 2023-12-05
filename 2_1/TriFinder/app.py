1a202e18dc8b91b3068752ac0f204abde60e83de
NnicanBuak <mybailengdb@gmail.com>
2023-12-05 11:35:36 +0800

    refactor app.py

--- 2_1/TriFinder/app.py
+++ 2_1/TriFinder/app.py
@@ -1,44 +1,51 @@
-from matplotlib.axes import Axes
+# Python >3.10
+
 from pycoordsplain.points import Point, PointsDraw
 from pycoordsplain.triangles import Triangle, TrianglesDraw, min_max_triangle
 
 
-from matplotlib.widgets import Button, TextBox
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import text
+from matplotlib.widgets import Button, TextBox
 from matplotlib.tri import Triangulation
 
+import re
 
-def read_pointslist_from_file(file_path: str):
+
+def read_pointslist_from_file(file_path: str) -> str | None:
     try:
         with open(file_path) as pointlist:
-            # "[0, 1][2, 5][3, 0]"" ...
             points.list = []
-            splitted_pointlist: list[str] = pointlist.read().strip("[]").split("][")
-            for splitted_point in splitted_pointlist:
+            for point_coordinates in re.findall(r"\[\d{1,}, \d{1,}]", pointlist.read()):
+                point_coordinates_list = point_coordinates.strip("[]").split(",")
                 points.add_point(
-                    int(splitted_point.split(",")[0]), int(splitted_point.split(",")[1])
+                    int(point_coordinates_list[0]), int(point_coordinates_list[1])
                 )
+
     except FileNotFoundError:
         print(f"Error: File {file_path} not found.")
-        return FileNotFoundError
+        return "File not found."
     except PermissionError:
         print(f"Error: Permission denied while trying to open {file_path}.")
-        return PermissionError
+        return "Error: Permission denied while trying to open file."
     except Exception as e:
         print(f"Error: An unexpected error occurred - {e}")
-        return e
+        return "An unexpected error occurred"
 
-def distance_squared(point1, point2):
+
+def distance_squared(point1: Point, point2: Point) -> int:
     return (point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2
 
 
 def on_findbutton_clicked(event) -> None:
     min_max_triangle(points.list)
 
+
 def on_pointlistpath_submit(event) -> None:
     global pointslist_input_buffer
     exception = read_pointslist_from_file(event)
     if exception:
+        error_text.set_text(exception)
         pointslist_input.set_val(pointslist_input_buffer)
         return
     pointslist_input_buffer = event
@@ -52,14 +59,19 @@ if __name__ == "__main__":
     plt.rcParams["figure.figsize"] = (8, 8)
 
     figure, axes = plt.subplots()
-    figure.suptitle("\n\nЗадача: Поиск max и min треугольников между заданными точками")
-    axes.set_title("Координатная плоскость")
-    axes.set_xlabel("Ось X")
-    axes.set_ylabel("Ось Y")
+    axes.set_title("Coordinate plane")
+    axes.set_xlabel("X-axis")
+    axes.set_ylabel("Y-axis")
     find_button = Button(plt.axes((0.9, 0.95, 0.1, 0.05)), "Поиск", color="g")
     find_button.on_clicked(on_findbutton_clicked)
-    pointslist_input = TextBox(plt.axes((0.18, 0.95, 0.3, 0.05)), "Путь к списку точек:", "./2_1/data/plist.txt")
+    pointslist_input = TextBox(
+        plt.axes((0.18, 0.95, 0.3, 0.05)),
+        "Путь к списку точек:",
+        "./2_1/data/plist.txt",
+    )
     pointslist_input.on_submit(on_pointlistpath_submit)
+    error_text = text(0, -0.4, "")
+    error_text.set_color("r")
 
     pointslist_input_buffer: str = pointslist_input.text
 
